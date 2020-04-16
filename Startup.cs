2b104d1aa928b7468ca5c9eb2e35b0638b0ef9e4
daesung626 <57124915+daesung626@users.noreply.github.com>
2020-04-16 10:30:34 +0900

    4/16/20

--- Startup.cs
+++ Startup.cs
@@ -36,14 +36,14 @@ namespace GSAPP
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            // if (env.IsDevelopment())
-            // {
-            app.UseDeveloperExceptionPage();
-            // }
-            // else
-            // {
-            //     app.UseExceptionHandler("/Home/Error");
-            // }
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
 
             app.UseStaticFiles();
             app.UseSession();
